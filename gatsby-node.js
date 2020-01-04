9da62a6d8a911180692dddb390a4ee5b6cf214f0
WataruFujiwara <wataru930@gmail.com>
2020-01-05 06:23:32 +0900

    change : get slug

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,13 @@
 // 新しいノードが作成（または更新）されるたびにGatsbyによって呼び出されます。
-exports.onCreateNode = ({ node }) => {
-    console.log(node.internal.type)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
   }
+}
