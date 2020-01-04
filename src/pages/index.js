25dfd994d3ec664ff37d8b4ac4116b8510a590de
WataruFujiwara <wataru930@gmail.com>
2020-01-05 06:05:30 +0900

    change :  sort

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,18 +44,18 @@ export default ({ data }) => {
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
+    allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+        totalCount
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+              date(formatString: "DD MMMM, YYYY")
+            }
+            excerpt
           }
-          excerpt
         }
       }
-    }
   }
 `
